218332137d7e838c37b9a748effb3c2d21c0e342
CoCo <eneluwecc@gmail.com>
2025-07-28 13:48:55 +0100

    Finished the Skills and Testimonial Section

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const body = document.body;
 
 const enableDarkmode = () => {
     body.classList.add('darkmode');
+    // document.getElementById('testimonial').style.backgroundImage = "url('images/testimonial-bg-drk.png')";
     localStorage.setItem('darkmode', 'active');
 }
 
 const disableDarkmode = () => {
     body.classList.remove('darkmode');
+    // document.getElementById('testimonial').style.backgroundImage = "url('images/testimonial-bg-lite.png')";
     localStorage.setItem('darkmode', 'null');
 }
 
